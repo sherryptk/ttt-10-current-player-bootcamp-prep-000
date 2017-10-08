71e3251e5f8d6cec1bd44f8e9b2fbd68a42104a2
Sherry <sherryptk@gmail.com>
2017-10-08 17:45:22 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+def turn_count(board)
+  count= 0
+    board.each do |move|
+      if move == "X" || move == "O"
+        count +=1
+      end
+    end
+  return count
+end
+
+def current_player(board)
+  player=nil
+  board.each do |move|
+    if move !=" "
+      player=move
+    end
+  end
+  
+    if player == "X"
+      return "O"
+    else
+      return "X"
+end
